7fa5cc41dd4580dcf089f7dd475d2d9ad5eed69a
Naveen2070 <naveenrameshcud@gmail.com>
2024-06-17 19:55:40 +0530

    adding backend

--- app/(content)/archive/@archive/[[...filter]]/page.js
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -7,6 +7,7 @@ import {
 } from '@/lib/news';
 import Link from 'next/link';
 
+
 export default function FilterNewsPage({ params }) {
   let links = getAvailableNewsYears();
   const filter = params.filter;
